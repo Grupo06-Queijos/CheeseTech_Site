0cedbb34efbc24214b25e0e26a84294b31fa3368
RafaelH31 <rafael.cebrian@sptech.school>
2023-05-16 09:48:05 -0300

    criando as rotas do cadastro-empresa

---
+++ site/src/controllers/empresaController.js
@@ -0,0 +1,79 @@
+var empresaModel = require("../models/empresaModel");
+
+var sessoes = [];
+
+function testar(req, res) {
+    console.log("ENTRAMOS NA empresaController");
+    res.json("ESTAMOS FUNCIONANDO!");
+}
+
+function listar(req, res) {
+    empresaModel.listar()
+        .then(function (resultado) {
+            if (resultado.length > 0) {
+                res.status(200).json(resultado);
+            } else {
+                res.status(204).send("Nenhum resultado encontrado!")
+            }
+        }).catch(
+            function (erro) {
+                console.log(erro);
+                console.log("Houve um erro ao realizar a consulta! Erro: ", erro.sqlMessage);
+                res.status(500).json(erro.sqlMessage);
+            }
+        );
+}
+
+function cadastrar(req, res) {
+    // Crie uma variável que vá recuperar os valores do arquivo cadastro.html
+    var cnpj = req.body.CNPJServer;
+    var empresa = req.body.empresaServer;
+    var emailemp = req.body.emailempServer;
+    var telefone = req.body.telefoneServer;
+    var cep = req.body.cepServer;
+    var camara = req.body.camaraServer;
+    // Faça as validações dos valores
+    if (cnpj == undefined) {
+        res.status(400).send("Seu cnpj está undefined!");
+
+    } else if (empresa == undefined) {
+        res.status(400).send("Seu empresa está undefined!");
+
+    } else if (emailemp == undefined) {
+        res.status(400).send("Seu emailemp está undefined!");
+    } 
+    else if (telefone == undefined) {
+        res.status(400).send("Seu telefone está undefined!");
+    } 
+    else if (cep == undefined) {
+        res.status(400).send("Seu cep está undefined!");
+    } 
+      else if (camara == undefined) {
+        res.status(400).send("Sua camara está undefined!");
+    } else {
+        
+        // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
+        empresaModel.cadastrar(cnpj, empresa, emailemp, telefone, cep, camara)
+            .then(
+                function (resultado) {
+                    res.json(resultado);
+                }
+            ).catch(
+                function (erro) {
+                    console.log(erro);
+                    console.log(
+                        "\nHouve um erro ao realizar o cadastro! Erro: ",
+                        erro.sqlMessage
+                    );
+                    res.status(500).json(erro.sqlMessage);
+                }
+            );
+    }
+}
+
+module.exports = {
+    entrar,
+    cadastrar,
+    listar,
+    testar
+}
---
+++ site/src/models/empresaModel.js
@@ -0,0 +1,29 @@
+var database = require("../database/config")
+
+function listar() {
+    console.log("ACESSEI O EMPRESA MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function listar()");
+    var instrucao = `
+        SELECT * FROM Empresa;
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucao);
+    return database.executar(instrucao);
+}
+
+// Coloque os mesmos parâmetros aqui. Vá para a var instrucao
+function cadastrar(cnpj, empresa, emailemp, telefone, cep, camara) {
+    console.log("ACESSEI O EMPRESA MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", cnpj, empresa, emailemp, telefone, cep, camara);
+    
+    // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
+    //  e na ordem de inserção dos dados.
+    var instrucao = `
+        INSERT INTO Empresa (CNPJ, Nome_Empresa, Email, Telefone, CEP, Camara) VALUES ('${cnpj}', '${empresa}', '${emailemp}', '${telefone}', '${cep}', '${camara}');
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucao);
+    return database.executar(instrucao);
+}
+
+module.exports = {
+    entrar,
+    cadastrar,
+    listar,
+};
